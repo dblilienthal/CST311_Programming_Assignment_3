92976b16c942c04aa91e4c9c2be51c2e48d12d27
None <None>
2020-04-16 19:22:53 -0700

    Initial Git upload using VIM

--- legacy_router_net.py
+++ legacy_router_net.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController
 from mininet.node import CPULimitedHost, Host, Node
--- linuxrouter.py
+++ linuxrouter.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 """
 linuxrouter.py: Example network with Linux IP router
 
